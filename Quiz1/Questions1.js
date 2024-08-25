0e6b3bb023fed882263cc101d58db4fe22ff051c
rkm-24 <163320625+rkm-24@users.noreply.github.com>
2024-08-25 21:33:48 +0530

    Add files via upload

---
+++ Quiz1/Questions1.js
@@ -0,0 +1,131 @@
+// creating an array to pass the questions with sl numbers and options
+let questions = [
+    {
+        num : 1,
+        question : "Raj Gopal ji Maharaj is the form of which Deity?",
+        answer : "Krishna",
+        options : [
+            "Shiva",
+            "Krishna",
+            "Brahma",
+            "Hanuman"
+        ]
+    },
+    {
+        num : 2,
+        question : "How many days the 'Brahmotsav' is being celebrated in Madanpur?",
+        answer : "5-days",
+        options : [
+                "3-days",
+                "4-days",
+                "5-days",
+                "None of the above",
+        ]
+
+    },
+
+    {
+        num : 3,
+        question : "Madanpur mandir is associated with which of the following?",
+        answer : "Rang ji ka mandir, Vrindavan",
+        options : [
+                "Radhavallabh ji ka mandir, Vrindavan",
+                "Rang ji ka mandir, Vrindavan",
+                "Behari ji ka mandir, Vrindavan",
+                "None of the above",
+        ]
+
+    },
+
+    {
+        num : 4,
+        question : "What is the name of 'Railway Station' of Madanpur?",
+        answer : "Kaurara Stations",
+        options : [
+                "Madanpur Station",
+                "Shikohabad Jn.",
+                "Firozabad Station.",
+                "Kaurara Stations",
+        ]
+
+    },
+
+    {
+        num : 5,
+        question : "Madanpur is near Which of the following railway lines?",
+        answer : "Northern Railways",
+        options : [
+                "Western Railways",
+                "Northern Railways",
+                "Central Railways",
+                "Eastern Railways",
+        ]
+
+    },
+
+    {
+        num : 6,
+        question : "Madanpur is situated in which district of Uttar Pradesh?",
+        answer : "Firozabad",
+        options : [
+                "Mainpuri",
+                "Agra",
+                "Firozabad",
+                "None of the above",
+        ]
+
+    },
+
+    {
+        num : 7,
+        question : "Which one of the following places is nearest place from Madanpur?",
+        answer : "Sirsaganj",
+        options : [
+                "Mainpuri",
+                "Sirsaganj",
+                "Shikohabad",
+                "Agra",
+        ]
+
+    },
+
+    {
+        num : 8,
+        question : "What is the approx. normal distance/time taken from Agra to Kaurara Rly Stn. (Tilyani-Madanpur) by road?",
+        answer : "approx. 78km (1hr 20min)",
+        options : [
+                "approx. 78km (1hr 20min)",
+                "approx. 100km (2hr 50min)",
+                "approx. 50km (1hr 50min)",
+                "approx. 120km (3hr 20min)",
+        ]
+
+    },
+
+    {
+        num : 9,
+        question : "Which is the world famous 'International-Tourist' place near Madanpur ?",
+        answer : "Agra",
+        options : [
+                "Kanpur",
+                "Lucknow",
+                "Shikohabad",
+                "Agra",
+        ]
+
+    },
+
+    {
+        num : 10,
+        question : "Major customs and pooja paddhati in Madanpur mandir are followed as per the guidelines of Rangji ka mandir because of whom?",
+        answer : "Shr Shri 1008 Swamiji of Rangji mandir",
+        options : [
+                "Madanpur family",
+                "Some mathurs of Madanpur",
+                "Shr Shri 1008 Swamiji of Rangji mandir",
+                "None of the above",
+        ]
+
+    },
+
+]
